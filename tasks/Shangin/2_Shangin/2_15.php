9817a021791c78cc83700bc0579917ee8db4016a
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-26 12:21:30 +0500

    Update 2_15.php

--- tasks/Shangin/2_Shangin/2_15.php
+++ tasks/Shangin/2_Shangin/2_15.php
@@ -1,20 +1,20 @@
 <?php
-	$a = 2 * (3 - 1);
-	$b = 6 - 2;
+    $a = 2 * (3 - 1);
+    $b = 6 - 2;
     $result = $a == $b;
-	var_dump($result);
+    var_dump($result);
 ?>
 <br>
 <?php
-	$a = 5 * (7 - 4);
-	$b = 1 + 2 + 7;
+    $a = 5 * (7 - 4);
+    $b = 1 + 2 + 7;
     $result = $a >= $b;
-	var_dump($result);
+    var_dump($result);
 ?>
 <br>
 <?php
-	$a = 2 ** 4;
-	$b = 4 ** 2;
+    $a = 2 ** 4;
+    $b = 4 ** 2;
     $result = $a != $b;
-	var_dump($result);
+    var_dump($result);
 ?>
