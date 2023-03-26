159433fd4916c9b8f68a1f405c1a1feb98974f58
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-26 12:13:22 +0500

    Update 0_12.php

--- tasks/Shangin/0_Shangin/0_12.php
+++ tasks/Shangin/0_Shangin/0_12.php
@@ -1,26 +1,26 @@
 <?php
-	echo '<i>курсивный</i>';
+    echo '<i>курсивный</i>';
 ?>
 <br>
 
 <?php
-	echo "1 <br> 2 <br> 3 <br> 4 <br> 5 <br> 6 <br> 7 <br> 8 <br> 9 <br>";
+    echo "1 <br> 2 <br> 3 <br> 4 <br> 5 <br> 6 <br> 7 <br> 8 <br> 9 <br>";
 ?>
 <br>
 <?php
-	echo '<a href="mef.png"></a>';
+    echo '<a href="mef.png"></a>';
 ?>
 <br>
 <?php
-	echo '<form method="post">
+    echo '<form method="post">
     <input type="text" name="id" />
     <input type="submit" value="Добавить" />
-  </form>';
+    </form>';
 ?>
 <br>
 <?php
-   echo "<form action="index.php" method="get">
-   <textarea name="address" rows="5" cols="40">evrtbvrtbrtbrtbrt</textarea>
-   <input type="submit" value="hit it!" />
- </form>";
+    echo "<form action="index.php" method="get">
+    <textarea name="address" rows="5" cols="40">evrtbvrtbrtbrtbrt</textarea>
+    <input type="submit" value="hit it!" />
+    </form>";
 ?>
