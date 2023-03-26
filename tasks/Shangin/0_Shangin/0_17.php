a5d903635e8b7485df69ce364de68f2393dd3708
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-26 11:51:54 +0500

    Update 0_17.php

--- tasks/Shangin/0_Shangin/0_17.php
+++ tasks/Shangin/0_Shangin/0_17.php
@@ -1,70 +1,70 @@
 <?php
-	$str = 'abcde';
+    $str = 'abcde';
 	
-	echo $str[0];
-	echo $str[2]; 
-	echo $str[4]; 
+    echo $str[0];
+    echo $str[2]; 
+    echo $str[4]; 
 ?>
 <br>
 <?php
-	$str = 'abcde';
-	$str[0] = 'e';
+    $str = 'abcde';
+    $str[0] = 'e';
     $str[1] = 'd';
     $str[2] = 'c';
     $str[3] = 'b';
     $str[4] = 'a';
 	
-	echo $str; 
+    echo $str; 
 ?>
 <br>
 <?php
-	$str = 'abcde';
-	$num = 3; 
+    $str = 'abcde';
+    $num = 3; 
 	
-	echo $str[$num]; 
+    echo $str[$num]; 
 ?>
 <br>
 <?php
-	$str = 'abcde';
-	echo $str[4]; 
+    $str = 'abcde';
+    echo $str[4]; 
 ?>
 <br>
 <?php
-	$str = 'abcde';
-	echo $str[3]; 
+    $str = 'abcde';
+    echo $str[3]; 
 ?>
 <br>
 <?php
-	$str = 'abcde';
-	echo $str[2]; 
+    $str = 'abcde';
+    echo $str[2]; 
 ?>
 <br>
 <?php
-	$str = '12345';
-	echo $str[0] + $str[1] + $str[2] + $str[3] + $str[4];
+    $str = '12345';
+    echo $str[0] + $str[1] + $str[2] + $str[3] + $str[4];
 ?>
 <br>
 <?php
-	$num = 12345;
-	$str = (string) $num;
+    $num = 12345;
+    $str = (string) $num;
 	
-	echo $str[0] + $str[1] + $str[2] + $str[3] + $str[4];
+    echo $str[0] + $str[1] + $str[2] + $str[3] + $str[4];
 ?>
 <br>
 <?php
-	$num = 12345;
-	$str = (string) $num;
+    $num = 12345;
+    $str = (string) $num;
 	
-	echo $str[0] * $str[1] * $str[2] * $str[3] * $str[4];
+    echo $str[0] * $str[1] * $str[2] * $str[3] * $str[4];
 ?>
 <br>
 <?php
-	$str = '12345';
-	$str[0] = '5';
+    $str = '12345';
+    $str[0] = '5';
     $str[1] = '4';
     $str[2] = '3';
     $str[3] = '2';
     $str[4] = '1';
 	
-	echo $str; 
+    echo $str; 
 ?>
