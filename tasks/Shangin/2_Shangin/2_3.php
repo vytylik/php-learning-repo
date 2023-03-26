2f85b1ad8c39ab53394261414fdebcc55843c1d5
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-26 12:24:58 +0500

    Update 2_3.php

--- tasks/Shangin/2_Shangin/2_3.php
+++ tasks/Shangin/2_Shangin/2_3.php
@@ -1,32 +1,32 @@
 <?php
-	$num = 3;
+    $num = 3;
 	
-	if ($num > 0 and $num < 5) {
-		echo '+';
-	} else {
-		echo '-';
-	}
+    if ($num > 0 and $num < 5) {
+	    echo '+';
+    } else {
+	    echo '-';
+    }
 ?>
 <br>
 <?php
-	$num = 15;
+    $num = 15;
 	
-	if ($num >= 10 and $num <= 20) {
-		echo '+';
-	} else {
-		echo '-';
-	}
+    if ($num >= 10 and $num <= 20) {
+	    echo '+';
+    } else {
+	    echo '-';
+    }
 ?>
 <br>
 <?php
-	$num1 = 1;
+    $num1 = 1;
     $num2 = 3;
 	
-	if ($num1 <= 1 and $num2 >= 3) {
-		echo '+';
-	} else {
-		echo '-';
-	}
+    if ($num1 <= 1 and $num2 >= 3) {
+	    echo '+';
+    } else {
+	    echo '-';
+    }
 ?>
 <br>
 
