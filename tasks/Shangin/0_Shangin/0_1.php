66db39c674de4549c56bb4581257ddc1a975c3db
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-26 12:06:36 +0500

    Update 0_1.php

--- tasks/Shangin/0_Shangin/0_1.php
+++ tasks/Shangin/0_Shangin/0_1.php
@@ -1,8 +1,8 @@
 <meta charset="utf-8">
 <?php
-	error_reporting(E_ALL);
-	ini_set('display_errors', 'on');
-	mb_internal_encoding('UTF-8');
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'on');
+    mb_internal_encoding('UTF-8');
 	
-	include 'code.php';
+    include 'code.php';
 ?>
