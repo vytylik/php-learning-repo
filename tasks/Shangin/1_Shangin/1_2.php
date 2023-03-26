c13a5933a10016c96b4c3e4ce28ac4aa50b76a34
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-26 12:15:38 +0500

    Update 1_2.php

--- tasks/Shangin/1_Shangin/1_2.php
+++ tasks/Shangin/1_Shangin/1_2.php
@@ -1,21 +1,21 @@
 <?php
-	$arr = [1 => 'a', 2 => 'b', 3 => 'c'];
+    $arr = [1 => 'a', 2 => 'b', 3 => 'c'];
     var_dump($arr);
 ?>
 <br>
 <?php
-	$arr = [1 => 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+    $arr = [1 => 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
 	
-	var_dump ($arr);
+    var_dump ($arr);
 ?>
 <br>
 <?php
-	$arr = ['name' => "Никита", 'surname' => "Шаньгин", 'patronymic' => "Юрьевич"];
-	echo $arr['surname'] . " " . $arr['name'] . " " . $arr['patronymic'];
+    $arr = ['name' => "Никита", 'surname' => "Шаньгин", 'patronymic' => "Юрьевич"];
+    echo $arr['surname'] . " " . $arr['name'] . " " . $arr['patronymic'];
 ?>
 <br>
 <?php
-	$arr = ['year' => 2023, 'month' => "march", 'day' => 22];
-	echo $arr['year'] . " " . $arr['month'] . " " . $arr['day'];
+    $arr = ['year' => 2023, 'month' => "march", 'day' => 22];
+    echo $arr['year'] . " " . $arr['month'] . " " . $arr['day'];
 ?>
 <br>
