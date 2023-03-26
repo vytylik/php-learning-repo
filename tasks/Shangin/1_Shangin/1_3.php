1437d7eec2c1606bc1ec9493c0e8990c1f8ccf62
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-26 12:15:52 +0500

    Update 1_3.php

--- tasks/Shangin/1_Shangin/1_3.php
+++ tasks/Shangin/1_Shangin/1_3.php
@@ -1,5 +1,5 @@
 <?php
-	$arr = [2023 => 'year', "march" => 'month', 22 => 'day'];
-	echo $arr[2023] . " " . $arr["march"] . " " . $arr[22];
+    $arr = [2023 => 'year', "march" => 'month', 22 => 'day'];
+    echo $arr[2023] . " " . $arr["march"] . " " . $arr[22];
 ?>
 <br>
