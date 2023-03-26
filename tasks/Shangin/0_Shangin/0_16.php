050b254c91a8e2878e7c0749bf127f072ad6e0d3
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-26 11:50:02 +0500

    Update 0_16.php

--- tasks/Shangin/0_Shangin/0_16.php
+++ tasks/Shangin/0_Shangin/0_16.php
@@ -1,19 +1,19 @@
 <?php
-	$test = '12345';
+    $test = '12345';
     var_dump((int) $test);
 ?>
 <br>
 <?php
-	$test = (float) '12.345';
-	var_dump($test); 
+    $test = (float) '12.345';
+    var_dump($test); 
 ?>
 <br>
 <?php
-	$test = '12.345';
+    $test = '12.345';
     var_dump((int) $test);
 ?>
 <br>
 <?php
-	$test = 12345;
+    $test = 12345;
     var_dump((string) $test);
 ?>
