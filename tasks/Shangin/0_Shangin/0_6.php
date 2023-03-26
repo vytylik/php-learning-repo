f2352b8b08b26f649e3e39fefb817a37363b63e1
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-26 12:10:28 +0500

    Update 0_6.php

--- tasks/Shangin/0_Shangin/0_6.php
+++ tasks/Shangin/0_Shangin/0_6.php
@@ -1,61 +1,61 @@
 <!-- <?php
-	$a = 5 + 5 * 3;
-	echo $a;
+    $a = 5 + 5 * 3;
+    echo $a;
 ?>
 <br>
 <?php
-	$a = 5 + 5 * 3 + 3;
-	echo $a;
+    $a = 5 + 5 * 3 + 3;
+    echo $a;
 ?>
 <br>
 <?php
-	$a = 8 / 2 + 2;
-	echo $a;
+    $a = 8 / 2 + 2;
+    echo $a;
 ?>
 <br>
 <?php
-	$a = 8 + 2 / 2;
-	echo $a;
+    $a = 8 + 2 / 2;
+    echo $a;
 ?>
 <br>
 <?php
-	$a = 8 / 2 * 2;
-	echo $a;
+    $a = 8 / 2 * 2;
+    echo $a;
 ?>
 <br>
 <?php
-	$a = 8 * 4 / 2 / 2;
-	echo $a;
+    $a = 8 * 4 / 2 / 2;
+    echo $a;
 ?>
 <br>
 <?php
-	$a = (2 + 3) * (2 + 3);
-	echo $a;
+    $a = (2 + 3) * (2 + 3);
+    echo $a;
 ?>
 <br>
 <?php
-	$a = (2 + 3) * 2 + 3;
-	echo $a;
+    $a = (2 + 3) * 2 + 3;
+    echo $a;
 ?>
 <br>
 <?php
-	$a = 2 * (2 + 4 * (3 + 1));
-	echo $a;
+    $a = 2 * (2 + 4 * (3 + 1));
+    echo $a;
 ?>
 <br>
 <?php
-	$a = 2 * 8 / 4;
-	echo $a;
+    $a = 2 * 8 / 4;
+    echo $a;
 ?>
 <br>
 <?php
-	$a = (2 * 8) / 4;
-	echo $a;
+    $a = (2 * 8) / 4;
+    echo $a;
 ?>
 <br>
 <?php
-	$a = 2 * (8 / 4);
-	echo $a;
+    $a = 2 * (8 / 4);
+    echo $a;
 ?> -->
 
 <?php
