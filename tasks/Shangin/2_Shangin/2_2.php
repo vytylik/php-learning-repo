35fc350b1a6aae0ab89c6a53bc416b2c77dede85
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-26 12:23:52 +0500

    Update 2_2.php

--- tasks/Shangin/2_Shangin/2_2.php
+++ tasks/Shangin/2_Shangin/2_2.php
@@ -1,111 +1,111 @@
 <!-- <?php
-	$test1 = '3';
-	$test2 = '3';
+    $test1 = '3';
+    $test2 = '3';
 		
-	if ($test1 == $test2) {
-		echo '+';
-	} else {
-		echo '-';
-	}
+    if ($test1 == $test2) {
+	    echo '+';
+    } else {
+	    echo '-';
+    }
 ?>
 <br>
 <?php
-	$test1 = '3';
-	$test2 = '3';
+    $test1 = '3';
+    $test2 = '3';
 		
-	if ($test1 === $test2) {
-		echo '+';
-	} else {
-		echo '-';
-	}
+    if ($test1 === $test2) {
+	    echo '+';
+    } else {
+	    echo '-';
+    }
 ?>
 <br>
 <?php
-	$test1 = 3;
-	$test2 = '3';
+    $test1 = 3;
+    $test2 = '3';
 		
-	if ($test1 == $test2) {
-		echo '+';
-	} else {
-		echo '-';
-	}
+    if ($test1 == $test2) {
+	    echo '+';
+    } else {
+	    echo '-';
+    }
 ?>
 <br>
 <?php
-	$test1 = 3;
-	$test2 = '3';
+    $test1 = 3;
+    $test2 = '3';
 		
-	if ($test1 === $test2) {
-		echo '+';
-	} else {
-		echo '-';
-	}
+    if ($test1 === $test2) {
+	    echo '+';
+    } else {
+	    echo '-';
+    }
 ?>
 <br>
 <?php
-	$test1 = 3;
-	$test2 = 3;
+    $test1 = 3;
+    $test2 = 3;
 		
-	if ($test1 === $test2) {
-		echo '+';
-	} else {
-		echo '-';
-	}
+    if ($test1 === $test2) {
+	    echo '+';
+    } else {
+	    echo '-';
+    }
 ?>
 <br>
 <?php
-	$test1 = '3';
-	$test2 = '3';
+    $test1 = '3';
+    $test2 = '3';
 		
-	if ($test1 != $test2) {
-		echo '+';
-	} else {
-		echo '-';
-	}
+    if ($test1 != $test2) {
+	    echo '+';
+    } else {
+	    echo '-';
+    }
 ?>
 <br>
 <?php
-	$test1 = '3';
-	$test2 = '3';
+    $test1 = '3';
+    $test2 = '3';
 		
-	if ($test1 !== $test2) {
-		echo '+';
-	} else {
-		echo '-';
-	}
+    if ($test1 !== $test2) {
+	    echo '+';
+    } else {
+	    echo '-';
+    }
 ?>
 <br>
 <?php
-	$test1 = 3;
-	$test2 = '3';
+    $test1 = 3;
+    $test2 = '3';
 		
-	if ($test1 != $test2) {
-		echo '+';
-	} else {
-		echo '-';
-	}
+    if ($test1 != $test2) {
+	    echo '+';
+    } else {
+	    echo '-';
+    }
 ?>
 <br>
 <?php
-	$test1 = 3;
-	$test2 = '3';
+    $test1 = 3;
+    $test2 = '3';
 		
-	if ($test1 !== $test2) {
-		echo '+';
-	} else {
-		echo '-';
-	}
+    if ($test1 !== $test2) {
+	    echo '+';
+    } else {
+	    echo '-';
+    }
 ?>
 <br>
 <?php
-	$test1 = 3;
-	$test2 = 2;
+    $test1 = 3;
+    $test2 = 2;
 		
-	if ($test1 !== $test2) {
-		echo '+';
-	} else {
-		echo '-';
-	}
+    if ($test1 !== $test2) {
+	    echo '+';
+    } else {
+	    echo '-';
+    }
 ?>
 <br> -->
 
