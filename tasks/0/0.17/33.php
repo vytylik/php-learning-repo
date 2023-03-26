8ec72bc1df18f13b70f133bcc696d08eb84a8493
BloodyWolff ggez <irina.shikunova@gmail.com>
2023-03-26 19:19:10 +0300

    Шикунов

---
+++ tasks/0/0.17/33.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    $str = 'abcde';
+    $str[0] = 'e';
+    $str[1] = 'd';
+    $str[2] = 'c';
+    $str[3] = 'b';
+    $str[4] = 'a';
+    print($str);
+?>
+</body>
+</html>
---
+++ tasks/0/0.17/38.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    $str = '12345';
+    print($str[0]+$str[1]+$str[2]+$str[3]+$str[4]);
+?>
+</body>
+</html>
