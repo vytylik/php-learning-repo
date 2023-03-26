c7a20113c54e295dc85fcb44538ffc66d2b99c21
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-26 12:19:49 +0500

    Update 2_10.php

--- tasks/Shangin/2_Shangin/2_10.php
+++ tasks/Shangin/2_Shangin/2_10.php
@@ -1,29 +1,29 @@
 <?php
-	$min = 18;
+    $min = 18;
 	
-	if ($min >= 0 and $min <= 14) {
-		echo '1 четверть';
-	}
+    if ($min >= 0 and $min <= 14) {
+    	echo '1 четверть';
+    }
 	
-	if ($min >= 15 and $min <= 29) {
-		echo '2 четверть';
-	}
+    if ($min >= 15 and $min <= 29) {
+	echo '2 четверть';
+    }
 	
-	if ($min >= 30 and $min <= 44) {
-		echo '3 четверть';
-	}
+    if ($min >= 30 and $min <= 44) {
+	echo '3 четверть';
+    }
 	
-	if ($min >= 45 and $min <= 59) {
-		echo '4 четверть';
-	}
+    if ($min >= 45 and $min <= 59) {
+	echo '4 четверть';
+    }
 ?>
 <br>
 <?php
-	$str = '123';
+    $str = '123';
 	
-	if (strlen($str) >= 3) {
-		echo strlen($str);
-	}   
+    if (strlen($str) >= 3) {
+	echo strlen($str);
+    }   
 ?>
 <br>
 <?php
