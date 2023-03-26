12787495aec96ec088ffed4228c19a4e41310136
ForTuna901 <77511018+ForTuna901@users.noreply.github.com>
2023-03-26 16:00:38 +0500

    Create 18.php

---
+++ tasks/Верхорубов Артём/0/18.php
@@ -0,0 +1,30 @@
+<?php
+    $num = 1;
+    $num = $num + 1;
+    $num = $num + 1;
+    echo $num;
+?>
+<br>
+<?php
+    $num = 1;
+    $num = $num + 2;
+    $num = $num + 3;
+    echo $num;
+?>
+<br>
+<?php
+<?php
+    $num = 47;
+    $num += 7;
+    $num -=18;
+    $num *=10;
+    $num /=15;
+    echo $num;
+?>
+<br>
+<?php
+    $str = 'a';
+    $str .='b';
+    $str .='c';
+    echo $str;
+?>
