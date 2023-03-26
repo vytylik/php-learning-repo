e2797c8c8a2ed1c69f831834eb5d09c151e18655
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-26 12:34:52 +0500

    Update 2_8.php

--- tasks/Shangin/2_Shangin/2_8.php
+++ tasks/Shangin/2_Shangin/2_8.php
@@ -1,17 +1,17 @@
 <?php
-	$test = 10;
+    $test = 10;
 	
-	if ($test == 10) {
-		echo 'верно';
-	}
+    if ($test == 10) {
+	echo 'верно';
+    }
 ?>
 <br>
 <?php
     $test = 0;
-	if ($test == 0) echo 'верно'; else echo 'неверно!';
+    if ($test == 0) echo 'верно'; else echo 'неверно!';
 ?>
 <br>
 <?php
-	if ($test == 0) echo 'верно';
+    if ($test == 0) echo 'верно';
 ?>
 <br>
