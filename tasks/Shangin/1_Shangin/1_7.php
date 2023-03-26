2eb3f0315fa81dbbb66fc7823ec7bce3e21c3171
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-26 12:16:58 +0500

    Update 1_7.php

--- tasks/Shangin/1_Shangin/1_7.php
+++ tasks/Shangin/1_Shangin/1_7.php
@@ -1,5 +1,5 @@
 <?php
-	$arr = [1, 2, 3, 4, 5];
+    $arr = [1, 2, 3, 4, 5];
     $key1 = 0;
     $key2 = 1;
     $result = $arr[$key1] + $arr[$key2];
@@ -7,7 +7,7 @@
 ?>
 <br>
 <?php
-	$arr = ['a' => 1, 'b' => 2, 'c' => 3];
+    $arr = ['a' => 1, 'b' => 2, 'c' => 3];
     $key = 1;
     var_dump ($key);
 ?>
