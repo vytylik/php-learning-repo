4d9c6d3818252fe2750a410e65b55e1b43f9f8aa
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-30 15:05:35 +0500

    Update 3_10.php

--- tasks/Shangin/3_Shangin/3_10.php
+++ tasks/Shangin/3_Shangin/3_10.php
@@ -1,25 +1,23 @@
 <?php
-	$arr = [1, 2, 3, 0, 5];
-	
-	foreach ($arr as $elem) {
-		if ($elem == 0) {
-			echo 'есть';
-			break; 
-		}
+    $arr = [1, 2, 3, 0, 5];
+    foreach ($arr as $elem) {
+	if ($elem == 0) {
+	    echo 'есть';
+	    break; 
 	}
+    }
 ?>
 <br>
 <?php
-	$arr = [1, 2, 3, 0, 5, -4, 6, -8];
+    $arr = [1, 2, 3, 0, 5, -4, 6, -8];
     $res = 0;
-	
-	foreach ($arr as $elem) {
-            $res += $elem;
-            if ($elem == -8) {
-            $res -= $elem;
-            break;
-		}
+    foreach ($arr as $elem) {
+    	     $res += $elem;
+             if ($elem == -8) {
+             $res -= $elem;
+             break;
 	}
+    }
     echo $res;
 ?>
 <br>
