f1e607462e39b7aedfb732bca0dbd0987b5af386
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-30 15:08:44 +0500

    Update 3_13.php

--- tasks/Shangin/3_Shangin/3_13.php
+++ tasks/Shangin/3_Shangin/3_13.php
@@ -1,12 +1,12 @@
 <?php
-	$num = 31;
+    $num = 31;
 	
-	$flag = true;
-	for ($i = 2; $i < $num; $i++) {
-		if ($num % $i === 0) {
-			$flag = false;
-			break; 
-		}
+    $flag = true;
+    for ($i = 2; $i < $num; $i++) {
+	if ($num % $i === 0) {
+	    $flag = false;
+            break; 
 	}
-	var_dump($flag);
+    }
+    var_dump($flag);
 ?>
