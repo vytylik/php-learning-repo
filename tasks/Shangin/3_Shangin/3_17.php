1ccb8e7901a46c75b35efe2bd2cb518cba58610b
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-30 15:14:03 +0500

    Update 3_17.php

--- tasks/Shangin/3_Shangin/3_17.php
+++ tasks/Shangin/3_Shangin/3_17.php
@@ -1,11 +1,11 @@
 <?php
-	$arr = [];
+    $arr = [];
 	
-	$arr[] = 1;
-	$arr[] = 2;
-	$arr[] = 3;
-	$arr[] = 4;
-	$arr[] = 5;
+    $arr[] = 1;
+    $arr[] = 2;
+    $arr[] = 3;
+    $arr[] = 4;
+    $arr[] = 5;
     $arr[] = 6;
     $arr[] = 7;
     $arr[] = 8;
@@ -14,32 +14,32 @@
 ?>
 <br>
 <?php
-	$arr = [];
+    $arr = [];
 	
-	$arr[] = "x";
-	$arr[] = "x";
-	$arr[] = "x";
-	$arr[] = "x";
-	$arr[] = "x";
+    $arr[] = "x";
+    $arr[] = "x";
+    $arr[] = "x";
+    $arr[] = "x";
+    $arr[] = "x";
 ?>
 <br>
 <?php
-	$arr = [];
+    $arr = [];
 	
-	for ($i = 0; $i <= 100; $i++) {
-		$arr[] = $i + 1;
-	}
+    for ($i = 0; $i <= 100; $i++) {
+	$arr[] = $i + 1;
+    }
 	
-	var_dump($arr);
+    var_dump($arr);
 ?>
 <br>
 <?php
-	$arr = [];
+    $arr = [];
 	
-	for ($i = 0; $i <= 99; $i = $i + 2) {
-		$arr[] = $i + 1;
-	}
+    for ($i = 0; $i <= 99; $i = $i + 2) {
+	$arr[] = $i + 1;
+    }
 	
-	var_dump($arr);
+    var_dump($arr);
 ?>
 <br>
