1439fee2da3a4ffc2049ac4907831743656a3b70
Nikita1234554321 <77499908+Nikita1234554321@users.noreply.github.com>
2023-03-30 15:12:45 +0500

    Update 3_16.php

--- tasks/Shangin/3_Shangin/3_16.php
+++ tasks/Shangin/3_Shangin/3_16.php
@@ -1,9 +1,9 @@
 <?php
-	for ($i = 1; $i <= 9; $i++) {
-		for ($j = 1; $j <= 3; $j++) {
-			echo $i;
-		}
+    for ($i = 1; $i <= 9; $i++) {
+	for ($j = 1; $j <= 3; $j++) {
+	    echo $i;
 	}
+    }
 ?>
 <br>
 <?php
